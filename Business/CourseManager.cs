db8d0b6c4ac0d7130d41bdbd4e236a30c8ffb74f
yasiraydn <yaasiraydin@gmail.com>
2024-03-02 20:54:39 +0300

    Add project files.

---
+++ Business/CourseManager.cs
@@ -0,0 +1,46 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+
+    Course[] courses=new Course[3];
+
+    //Constructor bloğu new leyince çalışır.
+    public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "C#";
+        course1.Description = ".NET 8";
+        course1.Price = 0;
+
+        Course course2 = new Course();
+        course2.Id = 1;
+        course2.Name = "C++";
+        course2.Description = "Falan filan";
+        course2.Price = 15;
+
+        Course course3 = new Course();
+        course3.Id = 1;
+        course3.Name = "Python";
+        course3.Description = "3.12";
+        course3.Price = 20;
+
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+
+    }
+    //Method deniyor bu komuta.
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 10000.5;
+
+bool isAuthenticated = true;
+//variebles --> camelCase
+
+Console.WriteLine(message1);
+Console.WriteLine(term);
+Console.WriteLine(amount);
+Console.WriteLine(isAuthenticated);
+
+
+//condition
+if (isAuthenticated == true)
+{
+    Console.WriteLine("Hoşgeldiniz");
+}
+else
+{
+    Console.WriteLine("Giriş yapınız");
+}
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3" };
+//string[] loans2 = new string[5];
+//loans2[0] = "Kredi 1";
+
+    //start     condition         increment
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+CourseManager courseManager = new CourseManager();
+Course[] courses2 = courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + "/" + courses2[i].Price);
+}
